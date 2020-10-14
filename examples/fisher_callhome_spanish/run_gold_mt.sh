66de6f52a13e5cb7e503a05f594fd8e915c3e2de
JINYI YANG <jinyiyang_mac@JINYIs-MacBook-Pro.local>
2020-10-14 14:14:18 -0400

    Add decode scripts

--- examples/fisher_callhome_spanish/run_gold_mt.sh
+++ examples/fisher_callhome_spanish/run_gold_mt.sh
@@ -3,16 +3,25 @@
 
 . cmd.sh
 . path.sh
-. conf/mt_gold_transformer_bpe.sh
+conf="conf/mt_gold_transformer_bpe.sh"
+
+. $conf
 
 stage=-1
-ngpus=2
+ngpus=3
 bin_dir=exp/gold_mt/bpe_bin
 exp_dir=exp/gold_mt
+dsets=("valid" "test" "test1" "test2" "test3" "test4")
+original_dsets=("train_dev" "fisher_dev" "fisher_dev2" "fisher_test" "callhome_devtest" "callhome_evltest")
+decode_mdl="checkpoint_best"
+bpe_type="sentencepiece"
+generate_bsz=256
+
 
 if [ $stage -le 0 ]; then
     echo "$(date) => training transfromer model"
     mkdir -p $exp_dir/log || exit 1
+    cp $conf $exp_dir
     $cuda_cmd --gpu $ngpus $exp_dir/log/train.log \
         fairseq-train $bin_dir \
         --num-workers $train_num_workers \
@@ -48,4 +57,23 @@ if [ $stage -le 0 ]; then
         --save-dir $exp_dir/checkpoints \
         --save-interval $save_interval \
         --log-format json || exit 1
+fi
+
+if [ $stage -le 1 ]; then
+    for idx in $(seq 0 $((${#dsets[@]}-1))); do
+        dset=${dsets[$idx]}
+        dset_name=${original_dsets[idx]}
+        decode_dir=$exp_dir/decode_${dset_name}
+        echo "$(date) => Decode $dset_name with $exp_dir/checkpoints/${decode_mdl}.pt"
+        mkdir -p $decode_dir || exit 1
+        $cuda_cmd --gpu 1 --mem 4G $decode_dir/log/decode.log \
+         fairseq-generate $bin_dir \
+            --task $task \
+            --gen-subset $dset \
+            --path $exp_dir/checkpoints/${decode_mdl}.pt \
+            --batch-size $generate_bsz \
+            --remove-bpe $bpe_type \
+            --num-workers $train_num_workers \
+            > $decode_dir/results_${decode_mdl}.txt || exit 1
+    done
 fi
