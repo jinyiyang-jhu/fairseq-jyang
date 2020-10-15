09c3646f3cc9169a123cad8eb0672161bca690d4
JINYI YANG <jinyiyang_mac@JINYIs-MacBook-Pro.local>
2020-10-15 00:25:58 -0400

    [wip] Add Kaldi and openfst path; need to remove it from final scripts (manually add by user case)

--- examples/fisher_callhome_spanish/path.sh
+++ examples/fisher_callhome_spanish/path.sh
@@ -1,2 +1,4 @@
 FAIRSEQ_PATH=$PWD/../../fairseq
-export PATH=$FAIRSEQ_PATH:$FAIRSEQ_PATH/utils:$PWD:$PATH
+KALDI_LATBIN_PATH=/export/b07/jyang/kaldi-jyang/kaldi/src/latbin
+OPENFST_PATH=/export/b07/jyang/kaldi-jyang/kaldi/tools/openfst/bin
+export PATH=$FAIRSEQ_PATH:$FAIRSEQ_PATH/utils:$KALDI_LATBIN_PATH:$OPENFST_PATH:$PWD:$PATH
