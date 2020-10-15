9ac42a15795211ee69a4dae03d712e1d8450bc56
JINYI YANG <jinyiyang_mac@JINYIs-MacBook-Pro.local>
2020-10-15 00:23:40 -0400

    Change the lattice2FST: from one utterance per file to one lat.gz per file

---
+++ examples/fisher_callhome_spanish/local/lattice_preprocess/fst2plf.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+systab=$1
+sos=$2
+eos=$3
+
+# This script read from STDIN (output from lattice-to-fst), and write to STDOUT
+{
+    uttid=""
+    while read -r; do
+      if [[ $REPLY =~ ^$ ]]; then
+        plf=$(echo "$block" |\
+          fstcompile --arc_type=log | \
+          fstpush --push_weights --remove_total_weight | \
+          fstconcat $sos - | \
+          fstconcat - $eos | \
+          fstrmepsilon | \
+          fstminimize | \
+          fsttopsort | \
+          fstprint --isymbols=$symtable --osymbols=$symtable | \
+          perl local/lattice_preprocess/txt2plf.pl)
+        echo -e "$uttid $plf"
+        block=""
+        uttid=""
+      elif [ "$uttid" == "" ]; then
+        uttid="$REPLY"
+      else
+        block+=$REPLY$'\n'
+      fi
+    done
+} < /dev/stdin
--- examples/fisher_callhome_spanish/local/lattice_preprocess/lattice2FST.sh
+++ examples/fisher_callhome_spanish/local/lattice_preprocess/lattice2FST.sh
@@ -1,28 +1,52 @@
 #!/bin/bash
- 
+
 # Derived from the Kaldi recipe for Fisher Spanish (KALDI_TRUNK/egs/fisher_callhome_spanish/s5/local)
- 
+
+cmd=run.pl
+[ -f ./path.sh ] && . ./path.sh;
+. cmd.sh
+. parse_options.sh || exit 1;
+
 # Use Kaldi tools to process lattice
-if [ $# -lt 2 ]; then
- echo "Usage : $0 <lat-dir>  <output-file>"
- exit 1
+if [ $# -ne 3 ]; then
+  echo "Usage : $0 <lat-dir>  <output-file> <word2int-file>"
+  echo "E.g.: $0 exp/decode_test exp/decode_test_plf data/lang/words.txt"
+  exit 1
 fi
-SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
- 
-. $SCRIPT_DIR/path.sh
- 
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 lat_dir=$1
 plf_dir=$2
+symtable=$3
+
+! which fstcompile \
+  && echo "OpenFST path not found! Please add openfst path to ./path.sh and run this script again !" && exit 1;
+! which lattice-to-fst \
+  && echo "Kaldi path not found! Please add kaldi path to ./path.sh and run this script again !" && exit 1;
+
+[ -f $symtable ] || (echo "$symtable does not exist !" && exit 1)
 acoustic_scale=1.0
-plf_file=$plf_dir/plf.txt
- 
-lat_dir=`realpath $lat_dir`
- 
-if [ ! -f $lat_dir/lat_file.index ]; then
- find $lat_dir -name "*.lat.ark" > $lat_dir/lat_file.index || exit 1
+lat_dir=$(realpath $lat_dir)
+
+nj=$(ls $lat_dir/lat.*.gz | wc -l)
+sos_exists=$(grep "<s>" $symtable)
+eos_exists=$(grep "</s>" $symtable)
+len_word_map=$(sort -n -k2 $symtable | tail -n1 | awk '{print $2}')
+
+if [ "$sos_exists" == "" ]; then # No "<s>" in the word to int mapping file
+  echo "Adding <s> to the word2int mapping file"
+  sos_index=$((len_word_map + 1))
+  echo -e "<s>\t${sos_index}" >> $symtable
 fi
- 
-[ -f $plf_file ] && rm $plf_file && echo "$plf_file exists, deleting it and rewriting it!"
+
+if [ "$sos_exists" == "" ]; then # No "<s>" in the word to int mapping file
+  echo "Adding </s> to the word2int mapping file"
+  eos_index=$((len_word_map + 2))
+  echo -e "<s>\t${eos_index}" >> $symtable
+fi
+
+mkdir -p $plf_dir || exit 1;
+
 cat > $plf_dir/sos.txt <<EOF
 1 2 <s> <s>
 2
@@ -31,38 +55,12 @@ cat > $plf_dir/eos.txt <<EOF
 1 2 </s> </s>
 2
 EOF
- 
-while read line; do
- dir=`dirname $line`
- lat_name=$(basename $line .lat.ark)
- 
- # We need to create a word2int map from given lattice.
- symtable=$plf_dir/$lat_name.map
- /apollo/bin/env -e Pryon lattice_symbol_table_tool $line $symtable
- max_symtable=`sort -n -k2 $symtable | tail -n1 | awk '{print $2}'`
- sos_index=$((max_symtable+1))
- eos_index=$((max_symtable+2))
- echo -e "<s>\t${sos_index}" >> $symtable
- echo -e "</s>\t${eos_index}" >> $symtable
- for prefix in sos eos; do
-   cat $plf_dir/$prefix.txt | \
-     fstcompile --isymbols=$symtable --osymbols=$symtable --arc_type=log > $plf_dir/$prefix.fst || exit 1
- done
- #fname=`echo $lat_name | sed 's/^\.\///g' | sed 's/\(.*\)-/\1\//g'`
- lattice-to-fst --lm-scale=1.0 --acoustic-scale=$acoustic_scale ark:$line ark,t:- \
-   | awk 'NR>1' | fstcompile --arc_type=log \
-   | fstpush --push_weights --remove_total_weight \
-   | fstconcat $plf_dir/sos.fst - | fstconcat - $plf_dir/eos.fst \
-   | fstrmepsilon | fstminimize | fsttopsort \
-   | fstprint --isymbols=$symtable --osymbols=$symtable \
-   | $SCRIPT_DIR/txt2plf.pl >> $plf_file || exit 1
- rm $symtable
- #rm $plf_dir/sos.fst
- #rm $plf_dir/eos.fst
-done < $lat_dir/lat_file.index
- 
+
 for prefix in sos eos; do
- rm $plf_dir/$prefix.fst
- rm $plf_dir/$prefix.txt
+  cat $plf_dir/$prefix.txt |
+    fstcompile --isymbols=$symtable --osymbols=$symtable --arc_type=log > $plf_dir/$prefix.fst || exit 1
 done
+$cmd JOB=1:$nj $plf_dir/log/lat2fst.JOB.log \
+  lattice-to-fst --lm-scale=1.0 --acoustic-scale=$acoustic_scale ark:"gunzip -c $lat_dir/lat.JOB.gz|" ark,t:- \| \
+  $SCRIPT_DIR/fst2plf.sh $symtable $plf_dir/sos.fst $plf_dir/eos.fst '>' $plf_dir/plf.JOB.txt || exit 1;
 echo "Succeeded in converting lattice to PLF format"
