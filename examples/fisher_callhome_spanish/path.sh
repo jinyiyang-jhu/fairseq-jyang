d7eee825c800ec5cd0952ee11c3dd9f46c884b95
Jinyi Yang <jyang126@jhu.edu>
2020-10-15 15:36:47 -0400

    Add parallel script path to path.sh

--- examples/fisher_callhome_spanish/path.sh
+++ examples/fisher_callhome_spanish/path.sh
@@ -1,4 +1,4 @@
 FAIRSEQ_PATH=$PWD/../../fairseq
 KALDI_LATBIN_PATH=/export/b07/jyang/kaldi-jyang/kaldi/src/latbin
 OPENFST_PATH=/export/b07/jyang/kaldi-jyang/kaldi/tools/openfst/bin
-export PATH=$FAIRSEQ_PATH:$FAIRSEQ_PATH/utils:$KALDI_LATBIN_PATH:$OPENFST_PATH:$PWD:$PATH
+export PATH=$FAIRSEQ_PATH:$FAIRSEQ_PATH/utils:$FAIRSEQ_PATH/utils/parallel:$KALDI_LATBIN_PATH:$OPENFST_PATH:$PWD:$PATH
