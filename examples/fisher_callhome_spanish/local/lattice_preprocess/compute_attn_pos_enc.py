3c8580e00e0d00e196f51c5a94748a56b6055e30
Jinyi Yang <jyang126@jhu.edu>
2020-10-26 23:38:46 -0400

    [wip] add utterance id in the lattice preprocesing; change BPE tokenizer in the PLF encoding script

--- examples/fisher_callhome_spanish/local/lattice_preprocess/compute_attn_pos_enc.py
+++ examples/fisher_callhome_spanish/local/lattice_preprocess/compute_attn_pos_enc.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 # Copyright (TBD)
 # License (TBD)
- 
+
 """
 This script calculates the attention masks and positional encodings from lattices
 for the transformer encoder.
@@ -12,109 +12,119 @@ import codecs
 import numpy as np
 from numpy import savez_compressed, asarray
 from apply_bpe import read_vocabulary, BPE
-from tqdm import tqdm, trange
 from lattice_utils import LatticeReader, LatticeNode, Lattice
- 
- 
-def tokenization_lat(word_seq, bpe_codes, bpe_vocab, bpe_gloss):
- """
- Tokenization for lattice text. Return the tokenized strings and id maps.
- """
- bpe = BPE(codes=bpe_codes, vocab=bpe_vocab, glossaries=bpe_gloss)
- split_tokens = []
- split_map = []
- word_seq = word_seq.strip().split()
- for i, word in enumerate(word_seq):
-   tokens_str = bpe.process_line(line=word)
-   tokens = tokens_str.split(' ')
-   split_tokens.extend([t for t in tokens])
-   split_map.extend([(i, n) for n in range(len(tokens))])
- return split_tokens, split_map
- 
- 
-def load_dataset(lattice_file, lattice_bpe_file, lat_pos_file, lat_prob_mask_file, bpe_codes, bpe_vocab, bpe_gloss,
-                probabilistic_masks=True, mask_direction=None, linearize=False):
- """
- Compute the attention masks and positional encoding from lattice.
- """
- with open(lattice_file) as lat_file, open(lattice_bpe_file, 'w') as lat_bpe:
-   output_pos = []
-   output_mask = []
-   lat_reader = LatticeReader()
-   for i, line in enumerate(tqdm(lat_file)):
-     line = line.strip()
-     lattice = lat_reader.read_sent(line, i)
-    
-     #lattice.plot(f"lattice_fwd", show_log_probs=["fwd_log_prob"])
-     #lattice.plot(f"lattice_bwd", show_log_probs=["bwd_log_prob"])
-     #lattice.plot(f"lattice_margin", show_log_probs=["marginal_log_prob"])
- 
-     utt = " ".join(lattice.str_tokens())
-     tokens, mapping = tokenization_lat(utt, bpe_codes, bpe_vocab, bpe_gloss)
-     nodes = []
-     node_map = {}
-     for j, (node_idx, n) in enumerate(mapping):
-       if node_idx not in node_map:
-         node_map[node_idx] = []
-       node_map[node_idx].append(j)
-      
-     for token, (node_idx, n) in zip(tokens, mapping):
-       orig_node = lattice.nodes[node_idx]
-       if n != 0:
-           nodes_prev = [len(nodes)-1]
-       else:
-           nodes_prev = [node_map[node][-1] for node in orig_node.nodes_prev]
-       if n != len(node_map[node_idx])-1:
-           nodes_next = [len(nodes)+1]
-       else:
-           nodes_next = [node_map[node][0] for node in orig_node.nodes_next]
-       node = LatticeNode(nodes_prev=nodes_prev, nodes_next=nodes_next, value=token,
-                          fwd_log_prob=orig_node.fwd_log_prob if n == 0 else 0,
-                          marginal_log_prob=orig_node.marginal_log_prob,
-                          bwd_log_prob=orig_node.bwd_log_prob if n == len(node_map[node_idx])-1 else 0)
-       nodes.append(node)
-     lattice_split = Lattice(idx=i, nodes=nodes)
-     if linearize:
-       pos = [i for i in range(len(lattice_split.nodes))]
-     else:
-       pos = lattice_split.longest_distances()
-       pos = [p+1 for p in pos]
-       log_conditional = lattice_split.compute_pairwise_log_conditionals(mask_direction, probabilistic_masks)[0]
-       print (' '.join(tokens), file=lat_bpe)
-       output_pos.append(pos)
-       output_mask.append(log_conditional)
-   final_pos = {f'{i}': np.array(output_pos[i]) for i in range(len(output_pos))}
-   final_mask = {f'{i}': np.array(output_mask[i]) for i in range(len(output_mask))}
-   savez_compressed(lat_pos_file, **final_pos)
-   savez_compressed(lat_prob_mask_file, **final_mask)
- 
-def main():
- parser = argparse.ArgumentParser()
- parser.add_argument('--lat_ifile', type=str, required=True, help='Input lattice PLF file name')
- parser.add_argument('--lat_bpe_file', type=str, required=True, help='Output lattice bpe file name')
- parser.add_argument('--lat_pos_file', type=str, required=True, help='Output lattice postion indexes')
- parser.add_argument('--lat_prob_mask', type=str, required=True, help='Output lattice probabilistic mask')
- parser.add_argument('--prob_mask_direction', type=str, default=None, choices=('None', 'fwd', 'bwd'),
-                     help='Output lattice mask direction')
- parser.add_argument('--bpe_code', type=str, required=True, help='Code file for BPE')
- parser.add_argument('--bpe_vocab', type=str, required=True,  help='BPE vocabulary')
- parser.add_argument('--bpe_gloss', type=str, default="<s> </s>",  help='BPE glossaries terms')
- parser.add_argument('--bpe_vocab_thres', type=str, default=1,help='BPE vocabulary frequency threshod')
- 
- 
- args = parser.parse_args()
- bpe_codes = codecs.open(args.bpe_code, encoding='utf-8')
- bpe_vocab = codecs.open(args.bpe_vocab, encoding='utf-8')
- bpe_vocab = read_vocabulary(bpe_vocab, args.bpe_vocab_thres)
- bpe_gloss = args.bpe_gloss.split()
- if args.prob_mask_direction == "None":
-   mask_direction = None
- else:
-   mask_direction = args.prob_mask_direction
-  
- load_dataset(args.lat_ifile, args.lat_bpe_file, args.lat_pos_file, args.lat_prob_mask,
-             bpe_codes, bpe_vocab, bpe_gloss, probabilistic_masks=True,
-             mask_direction=mask_direction, linearize=False)
-if __name__ == '__main__':
- main()
+import sentencepiece as spm
 
+def tokenization_lat(word_seqs, spm_processor, spm_format="piece"):
+    """
+    Tokenization for lattice text. Return the tokenized strings and id maps.
+    Args:
+    word_seqs (list): a list of words to be tokenized
+    sp_processor: loaded sentencepiece processor
+    spm_format (str): output tokenization format, "piece" or "id"
+    """
+    split_tokens = []
+    split_map = []
+
+    for i, word in enumerate(word_seqs):
+        if spm_format == "piece":
+            tokens = spm_processor.EncodeAsPieces(word)
+        elif spm_format == "id":
+            tokens = spm_processor.EncodeAsIds(word)
+        split_tokens.extend([t for t in tokens])
+        split_map.extend([(i, n) for n in range(len(tokens))])
+    import pdb
+    pdb.set_trace()
+    return split_tokens, split_map
+
+def load_dataset(spm_processor, lattice_file, lattice_bpe_file, spm_format="piece"):
+#def load_dataset(spm_processor, lattice_file, lattice_bpe_file, lat_pos_file, lat_prob_mask_file,
+ #                spm_format="piece", probabilistic_masks=True, mask_direction=None, linearize=False):
+    """
+    Compute the attention masks and positional encoding from lattice.
+    """
+    with open(lattice_file) as lat_file, open(lattice_bpe_file, 'w') as lat_bpe:
+        output_pos = []
+        output_mask = []
+        lat_reader = LatticeReader()
+        for i, line in enumerate(lat_file.readlines()):
+            line = line.strip()
+            tokens = line.split('\t')
+            uttid = tokens.pop(0)
+            lattice = lat_reader.read_sent(tokens[0], i)
+            tokens, mapping = tokenization_lat(
+                lattice.str_tokens(), spm_processor, spm_format=spm_format)
+            nodes = []
+            node_map = {}
+            for j, (node_idx, n) in enumerate(mapping):
+                if node_idx not in node_map:
+                    node_map[node_idx] = []
+                node_map[node_idx].append(j)
+
+            for token, (node_idx, n) in zip(tokens, mapping):
+                orig_node = lattice.nodes[node_idx]
+                if n != 0:
+                    nodes_prev = [len(nodes)-1]
+                else:
+                    nodes_prev = [node_map[node][-1]
+                                  for node in orig_node.nodes_prev]
+                if n != len(node_map[node_idx])-1:
+                    nodes_next = [len(nodes)+1]
+                else:
+                    nodes_next = [node_map[node][0]
+                                  for node in orig_node.nodes_next]
+                node = LatticeNode(nodes_prev=nodes_prev, nodes_next=nodes_next, value=token,
+                                   fwd_log_prob=orig_node.fwd_log_prob if n == 0 else 0,
+                                   marginal_log_prob=orig_node.marginal_log_prob,
+                                   bwd_log_prob=orig_node.bwd_log_prob if n == len(node_map[node_idx])-1 else 0)
+                nodes.append(node)
+            lattice_split = Lattice(idx=i, nodes=nodes)
+            if linearize:
+                pos = [i for i in range(len(lattice_split.nodes))]
+            else:
+                pos = lattice_split.longest_distances()
+                pos = [p+1 for p in pos]
+                log_conditional = lattice_split.compute_pairwise_log_conditionals(
+                    mask_direction, probabilistic_masks)[0]
+                print(uttid + '\t' + ' '.join(tokens), file=lat_bpe)
+                output_pos.append(pos)
+                output_mask.append(log_conditional)
+        final_pos = {f'{i}': np.array(
+            output_pos[i]) for i in range(len(output_pos))}
+        final_mask = {f'{i}': np.array(
+            output_mask[i]) for i in range(len(output_mask))}
+        savez_compressed(lat_pos_file, **final_pos)
+        savez_compressed(lat_prob_mask_file, **final_mask)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--spm_format', type=str, default="piece",
+                        help='Output spm format', choices=['piece', 'id'])
+    parser.add_argument('--spm_model', type=str, required=True,
+                        help='Trained sentencepiece model')
+    parser.add_argument('--lat_ifile', type=str, required=True,
+                        help='Input lattice PLF file name')
+    parser.add_argument('--lat_bpe_file', type=str,
+                        required=True, help='Output lattice bpe file name')
+    #parser.add_argument('--lat_pos_file', type=str,
+    #                    required=True, help='Output lattice postion indexes')
+    #parser.add_argument('--lat_prob_mask', type=str,
+    #                    required=True, help='Output lattice probabilistic mask')
+    #parser.add_argument('--prob_mask_direction', type=str, default=None, choices=('None', 'fwd', 'bwd'),
+    #                    help='Output lattice mask direction')
+
+    args = parser.parse_args()
+    #if args.prob_mask_direction == "None":
+    #    mask_direction = None
+    #else:
+    #    mask_direction = args.prob_mask_direction
+    sp = spm.SentencePieceProcessor()
+    sp.Load(args.spm_model)
+    load_dataset(sp, args.lat_ifile, args.lat_bpe_file)
+    #load_dataset(sp, args.lat_ifile, args.lat_bpe_file, args.lat_pos_file, args.lat_prob_mask,
+    #             spm_format=args.spm_format, probabilistic_masks=True, mask_direction=mask_direction, linearize=False)
+
+
+if __name__ == '__main__':
+    main()
--- examples/fisher_callhome_spanish/local/lattice_preprocess/run_lattice_preprocess.sh
+++ examples/fisher_callhome_spanish/local/lattice_preprocess/run_lattice_preprocess.sh
@@ -5,48 +5,54 @@
 # Make sure to run in pytorch_p36 env.
 
 stage=-1
+cmd=run.pl
 mask_direction="None" # "fwd", "bwd" or "None"
+
+[ -f ./path.sh ] && . ./path.sh;
+. cmd.sh
 . parse_options.sh || exit 1;
 
-lat_dir=$1 #/home/ec2-user/workspace/data/lattice_toy/lattice
-lat_plf_dir=$2 #/home/ec2-user/workspace/data/lattice_toy/lattice2plf
-bpe_code=$3 #/home/ec2-user/workspace/data/data_filtered_long_utts/bpe/code_file.txt
-bpe_vocab=$4 #/home/ec2-user/workspace/data/data_filtered_long_utts/bpe/vocab.all.txt
- 
+# Use Kaldi tools to process lattice
+if [ $# -ne 4 ]; then
+    echo "Usage : $0 <old-lat-dir> <new-plf-dir> <word2int> <bpe-model>"
+    echo "E.g.: $0 data/lattice data/plf data/lang/words.txt data/bpe/train.en.model"
+    exit 1
+fi
+
+lat_dir=$1
+lat_plf_dir=$2
+word_map=$3
+bpe_model=$4
+#bpe_code=$3 #/home/ec2-user/workspace/data/data_filtered_long_utts/bpe/code_file.txt
+#bpe_vocab=$4 #/home/ec2-user/workspace/data/data_filtered_long_utts/bpe/vocab.all.txt
+
 lat_processed=$new_lat_dir/plf_processed/
 
 if [ $stage -le 0 ]; then
-  echo "$(date -u): converting lattices to FSTs"
-  bash local/lattice_preprocess/lattice2FST.sh $lat_dir $lat_plf_dir/plf_edge $word_map || exit 1;
+    echo "$(date -u): converting lattices to FSTs"
+    bash local/lattice_preprocess/lattice2FST.sh --cmd "$train_cmd" \
+    $lat_dir $lat_plf_dir/plf_edge $word_map || exit 1;
 fi
 
 if [ $stage -le 1 ]; then
-# Edge lattice to node lattice
-  echo "`date`: converting PLF to node PLFs"
-  nj=$(ls $lat_plf_dir/plf.*.txt | wc -l)
-  mkdir -p $lat_plf_dir/plf_node || exit 1;
-  $cmd JOB=1:$nj python local/lattice_preprocess/preproc-lattice.py \
-    $lat_plf_dir/plf_edge/plf.JOB.txt $lat_plf_dir/plf_node/plf.node.JOB.txt
+    # Edge lattice to node lattice
+    echo "$(date -u): converting edge PLF to node PLFs"
+    nj=$(ls $lat_plf_dir/plf_edge/plf.*.txt | wc -l)
+    mkdir -p $lat_plf_dir/plf_node || exit 1;
+    $cmd JOB=1:$nj $lat_plf_dir/plf_node/log/edge2node.JOB.log \
+        python local/lattice_preprocess/preproc-lattice.py \
+        $lat_plf_dir/plf_edge/plf.JOB.txt $lat_plf_dir/plf_node/plf.node.JOB.txt
 fi
-# TODO: handle uttid in preproc-lattice.py
-# TODO: handle uttid in 
 
-# Filter the empty lattices in plf.node.tmp.txt, files not in the new selected_utt.index will be later removed
-# from transcripts and asr 1best
-grep -v "\[\]" $lat_plf_dir/plf.node.tmp.txt > $lat_plf_dir/plf.node.txt
-awk 'BEGIN{print "-1"}{if ($0 == "[],[]"){print NR}}' $lat_plf_dir/plf.node.tmp.txt \
-| awk 'NR==FNR{arr[$1]=1}(NR != FNR && (!(arr[FNR]))){print $0}'  - $lat_dir/selected_utt.index \
-> $lat_plf_dir/selected_utt.index
-rm $lat_plf_dir/plf.node.tmp.txt
- 
+if [ $stage -le 2 ]; then
 # Compute pos and mask
-echo "`date`: computing pos indice and prob masks from node PLFs"
-mkdir -p $lat_processed || exit 1
-/home/ec2-user/anaconda3/envs/pytorch_p36/bin/python3.6 $SCRIPT_DIR/compute_attn_pos_enc.py --lat_ifile $lat_plf_dir/plf.node.txt \
- --lat_bpe_file $lat_processed/plf.BPE.txt \
- --lat_pos_file $lat_processed/plf.pos.npz \
- --lat_prob_mask $lat_processed/plf.mask.npz \
- --prob_mask_direction $mask_direction \
- --bpe_code $bpe_code \
- --bpe_vocab $bpe_vocab || exit 1
- 
+    echo "$(date -u): getting BPE tokens, pos indice and probability matrices from PLFs"
+    mkdir -p $lat_processed || exit 1
+    $cmd JOB=1:$nj $lat_processed/log/plfinfo.JOB.log \
+    python $SCRIPT_DIR/compute_attn_pos_enc.py --lat_ifile $lat_plf_dir/plf.node.JOB.txt \
+        --lat_bpe_file $lat_processed/plf.BPE.JOB.txt \
+        --lat_pos_file $lat_processed/plf.pos.JOB.npz \
+        --lat_prob_mask $lat_processed/plf.mask.JOB.npz \
+        --prob_mask_direction $mask_direction \
+        --bpe_code $bpe_code \
+        --bpe_vocab $bpe_vocab || exit 1
