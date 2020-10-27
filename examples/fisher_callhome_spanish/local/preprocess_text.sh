aaee659a38856377860e499b8eb6d8403a965c87
Jinyi Yang <jyang126@jhu.edu>
2020-10-26 23:36:53 -0400

    Separate BPE training with encoding; add BPE steps in the main script

--- examples/fisher_callhome_spanish/local/preprocess_text.sh
+++ examples/fisher_callhome_spanish/local/preprocess_text.sh
@@ -3,25 +3,41 @@
 # This script perform BPE tokenization for train/dev/eval data.
 # This BPE model and data are provided from ESPNET recipe.
 stage=-1
-espnet_data=data/espnet_prepared
-bpemodel=exp/espnet_bpe_model/train_sp.en_bpe1000_lc.rm.model
-datadir=data
 source_lang="es"
 target_lang="en"
 case=".lc.rm"
 preprocess_num_workers=40
 
-exp_dir=exp/gold_mt
-bpedir=$datadir/bpe
+[ -f ./path.sh ] && . ./path.sh;
+. parse_options.sh || exit 1;
+
+if [ $# -ne 4 ]; then
+    echo "Usage: $0 <bpe-model> <input-data-dir> <output-data-dir> <exp-dir>"
+    echo "E.g.: $0 exp/bpe_es_en_lc/bpe_1000.lc.model data/espnet_prepared data/gold_mt exp/gold_mt"
+    exit 1
+fi
+
+bpemodel=$1
+idata_dir=$2 # contains "train train_dev fisher_dev fisher_dev2 fisher_test callhome_devtest callhome_evltest"
+odata_dir=$3
+exp_dir=$4
+
+bpedir=$odata_dir/bpe
 
 if [ $stage -le 0 ]; then
     [ -d $bpedir ] || mkdir $bpedir || exit 1
     for d in train train_dev fisher_dev fisher_dev2 fisher_test callhome_devtest callhome_evltest; do
-        echo "$(date -u) Processing BPE tokenization for dataset $d"
-        for lan in $source_lang $target_lang; do
-            input_file=$espnet_data/$d.$lan/text${case}
-            bash local/preprocess_bpe.sh $input_file $bpemodel $bpedir/$d.$lan || exit 1
-        done
+        if [ -d $idata_dir/$d.$lan ]; then
+            echo "$(date -u) Processing BPE tokenization for dataset $d"
+            for lan in $source_lang $target_lang; do
+                input_file=$idata_dir/$d.$lan/text${case}
+                cut -f 2- -d " " $input_file |\
+                    spm_encode --model=$bpemodel --output_format=piece |\
+                    cut -f 2- -d " " > $bpedir/$d.$lan || exit 1;
+            done
+        else
+            echo "$(date -u) Warning: escaping $d since $idata_dir/$d.$lan does not exist !"
+        fi
     done
 fi
 
--- examples/fisher_callhome_spanish/run_gold_mt.sh
+++ examples/fisher_callhome_spanish/run_gold_mt.sh
@@ -9,17 +9,38 @@ conf="conf/mt_gold_transformer_bpe.sh"
 
 stage=-1
 ngpus=3
-bin_dir=exp/gold_mt/bpe_bin
-exp_dir=exp/gold_mt
-dsets=("valid" "test" "test1" "test2" "test3" "test4")
-original_dsets=("train_dev" "fisher_dev" "fisher_dev2" "fisher_test" "callhome_devtest" "callhome_evltest")
 decode_mdl="checkpoint_best"
 bpe_type="sentencepiece"
 generate_bsz=256
 
+# Dataset dir/names
+original_datadir=data/espnet_prepared
+orginal_bpedir=data/gold_mt/bpe
+original_dsets=("train_dev" "fisher_dev" "fisher_dev2" "fisher_test" "callhome_devtest" "callhome_evltest")
+dsets=("valid" "test" "test1" "test2" "test3" "test4")
+bin_dir=exp/gold_mt/bpe_bin
+exp_dir=exp/gold_mt
+# BPE related path
+nbpe=1000
+case="lc.rm"
+bpe_train_text=exp/espnet_bpe_model/input.txt
+bpe_model_dir=exp/bpe_es_en_lc
+non_lan_syms=data/lang/en_es_non_lang_syms_lc.txt
+
 
 if [ $stage -le 0 ]; then
-    echo "$(date) => training transfromer model"
+    echo "$(date -u) => training BPE model"
+    bash local/train_bpe.sh $bpe_train_text $non_lan_syms $bpe_model_dir || exit 1;
+fi
+
+if [ $stage -le 1 ]; then
+    echo "$(date -u) => preprocessing datasets"
+    bpemodel=$bpe_model_dir/bpe_${nbpe}_${case}.model
+    bash local/preprocess_text.sh $bpemodel $original_datadir $orginal_bpedir $exp_dir
+fi
+
+if [ $stage -le 2 ]; then
+    echo "$(date -u) => training transfromer model"
     mkdir -p $exp_dir/log || exit 1
     cp $conf $exp_dir
     $cuda_cmd --gpu $ngpus $exp_dir/log/train.log \
@@ -59,12 +80,12 @@ if [ $stage -le 0 ]; then
         --log-format json || exit 1
 fi
 
-if [ $stage -le 1 ]; then
+if [ $stage -le 3 ]; then
     for idx in $(seq 0 $((${#dsets[@]}-1))); do
         dset=${dsets[$idx]}
         dset_name=${original_dsets[idx]}
         decode_dir=$exp_dir/decode_${dset_name}
-        echo "$(date) => Decode $dset_name with $exp_dir/checkpoints/${decode_mdl}.pt"
+        echo "$(date -u) => decoding $dset_name with $exp_dir/checkpoints/${decode_mdl}.pt"
         mkdir -p $decode_dir || exit 1
         $cuda_cmd --gpu 1 --mem 4G $decode_dir/log/decode.log \
          fairseq-generate $bin_dir \
@@ -75,5 +96,7 @@ if [ $stage -le 1 ]; then
             --remove-bpe $bpe_type \
             --num-workers $train_num_workers \
             > $decode_dir/results_${decode_mdl}.txt || exit 1
+        echo "$(date -u) => scoring BLEU for $dset_name with MOSES tools"
+        # TODO
     done
 fi
