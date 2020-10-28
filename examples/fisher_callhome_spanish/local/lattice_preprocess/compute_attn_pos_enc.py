a9d4f33f7104534b654e109973eb9610cd85bc9d
Jinyi Yang <jyang126@jhu.edu>
2020-10-28 00:29:49 -0400

    Use subword-nmt to encode PLF; add utterance ids to be used as npz file keys

--- examples/fisher_callhome_spanish/local/lattice_preprocess/compute_attn_pos_enc.py
+++ examples/fisher_callhome_spanish/local/lattice_preprocess/compute_attn_pos_enc.py
@@ -13,39 +13,41 @@ import numpy as np
 from numpy import savez_compressed, asarray
 from apply_bpe import read_vocabulary, BPE
 from lattice_utils import LatticeReader, LatticeNode, Lattice
-import sentencepiece as spm
 
-def tokenization_lat(word_seqs, spm_processor, spm_format="piece"):
+
+def tokenization_lat(word_seq, bpe_codes, bpe_vocab, bpe_gloss):
     """
     Tokenization for lattice text. Return the tokenized strings and id maps.
     Args:
-    word_seqs (list): a list of words to be tokenized
-    sp_processor: loaded sentencepiece processor
-    spm_format (str): output tokenization format, "piece" or "id"
+    word_seq (list): a list of words (str)
+    bpe_codes (codecs.StreamReaderWriter): bpe code file
+    bpe_vocab (codecs.StreamReaderWriter): bpe vocabulary file
+    bpe_gloss (str): a file contains bpe glossaries
     """
     split_tokens = []
     split_map = []
 
-    for i, word in enumerate(word_seqs):
-        if spm_format == "piece":
-            tokens = spm_processor.EncodeAsPieces(word)
-        elif spm_format == "id":
-            tokens = spm_processor.EncodeAsIds(word)
+    with open(bpe_gloss, 'r') as f:
+        glossaries = f.readlines()
+    glossaries = [x.strip() for x in glossaries]
+    bpe = BPE(codes=bpe_codes, vocab=bpe_vocab, glossaries=glossaries)
+
+    for i, word in enumerate(word_seq):
+        tokens_str = bpe.process_line(line=word)
+        tokens = tokens_str.split(' ')
         split_tokens.extend([t for t in tokens])
         split_map.extend([(i, n) for n in range(len(tokens))])
-    import pdb
-    pdb.set_trace()
     return split_tokens, split_map
 
-def load_dataset(spm_processor, lattice_file, lattice_bpe_file, spm_format="piece"):
-#def load_dataset(spm_processor, lattice_file, lattice_bpe_file, lat_pos_file, lat_prob_mask_file,
- #                spm_format="piece", probabilistic_masks=True, mask_direction=None, linearize=False):
+def load_dataset(lattice_file, lattice_bpe_file, lat_pos_file, lat_prob_mask_file,
+                bpe_codes, bpe_vocab, bpe_gloss, probabilistic_masks=True, 
+                mask_direction=None, linearize=False):
     """
     Compute the attention masks and positional encoding from lattice.
     """
     with open(lattice_file) as lat_file, open(lattice_bpe_file, 'w') as lat_bpe:
-        output_pos = []
-        output_mask = []
+        output_pos = {}
+        output_mask = {}
         lat_reader = LatticeReader()
         for i, line in enumerate(lat_file.readlines()):
             line = line.strip()
@@ -53,7 +55,7 @@ def load_dataset(spm_processor, lattice_file, lattice_bpe_file, spm_format="piec
             uttid = tokens.pop(0)
             lattice = lat_reader.read_sent(tokens[0], i)
             tokens, mapping = tokenization_lat(
-                lattice.str_tokens(), spm_processor, spm_format=spm_format)
+                lattice.str_tokens(), bpe_codes, bpe_vocab, bpe_gloss)
             nodes = []
             node_map = {}
             for j, (node_idx, n) in enumerate(mapping):
@@ -87,44 +89,38 @@ def load_dataset(spm_processor, lattice_file, lattice_bpe_file, spm_format="piec
                 log_conditional = lattice_split.compute_pairwise_log_conditionals(
                     mask_direction, probabilistic_masks)[0]
                 print(uttid + '\t' + ' '.join(tokens), file=lat_bpe)
-                output_pos.append(pos)
-                output_mask.append(log_conditional)
-        final_pos = {f'{i}': np.array(
-            output_pos[i]) for i in range(len(output_pos))}
-        final_mask = {f'{i}': np.array(
-            output_mask[i]) for i in range(len(output_mask))}
-        savez_compressed(lat_pos_file, **final_pos)
-        savez_compressed(lat_prob_mask_file, **final_mask)
-
+                output_pos[uttid] = np.array(pos)
+                output_mask[uttid] = np.array(log_conditional)
+        savez_compressed(lat_pos_file, **output_pos)
+        savez_compressed(lat_prob_mask_file, **output_mask)
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--spm_format', type=str, default="piece",
-                        help='Output spm format', choices=['piece', 'id'])
-    parser.add_argument('--spm_model', type=str, required=True,
-                        help='Trained sentencepiece model')
-    parser.add_argument('--lat_ifile', type=str, required=True,
-                        help='Input lattice PLF file name')
-    parser.add_argument('--lat_bpe_file', type=str,
-                        required=True, help='Output lattice bpe file name')
-    #parser.add_argument('--lat_pos_file', type=str,
-    #                    required=True, help='Output lattice postion indexes')
-    #parser.add_argument('--lat_prob_mask', type=str,
-    #                    required=True, help='Output lattice probabilistic mask')
-    #parser.add_argument('--prob_mask_direction', type=str, default=None, choices=('None', 'fwd', 'bwd'),
-    #                    help='Output lattice mask direction')
-
+    parser.add_argument('--lat_ifile', type=str, required=True, help='Input lattice PLF file name')
+    parser.add_argument('--lat_bpe_file', type=str, required=True, help='Output lattice bpe file name')
+    parser.add_argument('--lat_pos_file', type=str, required=True, help='Output lattice postion indexes')
+    parser.add_argument('--lat_prob_mask', type=str, required=True, help='Output lattice probabilistic mask')
+    parser.add_argument('--prob_mask_direction', type=str, default=None, choices=('None', 'fwd', 'bwd'),
+                        help='Output lattice mask direction')
+    parser.add_argument('--bpe_code', type=str, required=True, help='Code file for BPE')
+    parser.add_argument('--bpe_vocab', type=str, required=True,  help='BPE vocabulary')
+    parser.add_argument('--bpe_gloss', type=str, required=True, help='BPE glossaries terms')
+    parser.add_argument('--bpe_vocab_thres', type=str, default=1,help='BPE vocabulary frequency threshod')
+    
     args = parser.parse_args()
-    #if args.prob_mask_direction == "None":
-    #    mask_direction = None
-    #else:
-    #    mask_direction = args.prob_mask_direction
-    sp = spm.SentencePieceProcessor()
-    sp.Load(args.spm_model)
-    load_dataset(sp, args.lat_ifile, args.lat_bpe_file)
-    #load_dataset(sp, args.lat_ifile, args.lat_bpe_file, args.lat_pos_file, args.lat_prob_mask,
-    #             spm_format=args.spm_format, probabilistic_masks=True, mask_direction=mask_direction, linearize=False)
+    bpe_codes = codecs.open(args.bpe_code, encoding='utf-8')
+    bpe_vocab = codecs.open(args.bpe_vocab, encoding='utf-8')
+    bpe_vocab = read_vocabulary(bpe_vocab, args.bpe_vocab_thres)
+    bpe_gloss = args.bpe_gloss
 
+    if args.prob_mask_direction == "None":
+        mask_direction = None
+    else:
+        mask_direction = args.prob_mask_direction
+
+    load_dataset(args.lat_ifile, args.lat_bpe_file, args.lat_pos_file, args.lat_prob_mask,
+        bpe_codes, bpe_vocab, bpe_gloss, probabilistic_masks=True,
+        mask_direction=mask_direction, linearize=False)
 
 if __name__ == '__main__':
     main()
