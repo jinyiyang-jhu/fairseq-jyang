199b2f01df6233ebf34e91c3b43fd346c903a85c
Jinyi Yang <jyang126@jhu.edu>
2020-10-29 15:37:22 -0400

    Fix broken pip in gold mt text preprocess

--- examples/fisher_callhome_spanish/local/preprocess_text_subword_nmt.sh
+++ examples/fisher_callhome_spanish/local/preprocess_text_subword_nmt.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # This script perform BPE tokenization for train/dev/eval data.
-# This BPE model and data are provided from ESPNET recipe.
+# The datasets are provided from ESPNET recipe.
+
 stage=-1
 source_lang="es"
 target_lang="en"
@@ -19,27 +20,25 @@ fi
 
 bpe_code_dir=$1
 idata_dir=$2 # contains "train train_dev fisher_dev fisher_dev2 fisher_test callhome_devtest callhome_evltest"
-odata_dir=$3
+bpe_dir=$3
 exp_dir=$4
 
-bpedir=$odata_dir/bpe
-
 if [ $stage -le 0 ]; then
-    [ -d $bpedir ] || mkdir $bpedir || exit 1
+    [ -d $bpe_dir ] || mkdir -p $bpe_dir || exit 1
     for d in train train_dev fisher_dev fisher_dev2 fisher_test callhome_devtest callhome_evltest; do
-        if [ -d $idata_dir/$d.$lan ]; then
-            echo "$(date) Processing BPE tokenization for dataset $d"
-            for lan in $source_lang $target_lang; do
+        for lan in $source_lang $target_lang; do
+            if [ -d $idata_dir/$d.$lan ]; then
+                echo "$(date) Processing BPE tokenization for dataset $d.$lan"
                 input_file=$idata_dir/$d.$lan/text${case}
                 cut -f 2- -d " " $input_file | sed -e 's/\&apos\;/ \&apos\; /g' |\
                     subword-nmt apply-bpe -c $bpe_code_dir/code.txt \
-                    --vocabulary $bpe_code_dir/vocab.all.txt \
-                    --glossaries "$(cat ${bpe_code_dir}/glossaries.txt)" 
-                    --vocabulary-threshold 1 $ref > $ref_bpe || exit 1
-            done
-        else
-            echo "$(date) Warning: escaping $d since $idata_dir/$d.$lan does not exist !"
-        fi
+                        --vocabulary $bpe_code_dir/vocab.all.txt \
+                        --glossaries "$(cat ${bpe_code_dir}/glossaries.txt)" \
+                        --vocabulary-threshold 1 > $bpe_dir/$d.$lan || exit 1
+            else
+                echo "$(date) Warning: escaping $d since $idata_dir/$d.$lan does not exist !"
+            fi
+        done
     done
 fi
 
@@ -48,8 +47,8 @@ if [ $stage -le 1 ]; then
     preprocess_dir=$exp_dir/bpe_bin
     [ -d $preprocess_dir ] || mkdir -p $preprocess_dir || exit 1
     fairseq-preprocess --source-lang $source_lang --target-lang $target_lang \
-        --trainpref $bpedir/train --validpref $bpedir/train_dev \
-        --testpref $bpedir/fisher_dev,$bpedir/fisher_dev2,$bpedir/fisher_test,$bpedir/callhome_devtest,$bpedir/callhome_evltest \
+        --trainpref $bpe_dir/train --validpref $bpe_dir/train_dev \
+        --testpref $bpe_dir/fisher_dev,$bpe_dir/fisher_dev2,$bpe_dir/fisher_test,$bpe_dir/callhome_devtest,$bpe_dir/callhome_evltest \
         --destdir $preprocess_dir --joined-dictionary --append-eos-src --append-eos-tgt \
         --workers $preprocess_num_workers
 fi
