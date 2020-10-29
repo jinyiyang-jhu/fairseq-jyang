7cabe3e1d7f845f1152c4ff6d5a0a69f390fcfb8
Jinyi Yang <jyang126@jhu.edu>
2020-10-28 22:04:42 -0400

    Add Kaldi copy-matrix bin to the path

--- examples/fisher_callhome_spanish/path.sh
+++ examples/fisher_callhome_spanish/path.sh
@@ -1,6 +1,6 @@
 FAIRSEQ_PATH=$PWD/../../fairseq
-KALDI_LATBIN_PATH=/export/b07/jyang/kaldi-jyang/kaldi/src/latbin
+KALDI_PATH=/export/b07/jyang/kaldi-jyang/kaldi/src
 OPENFST_PATH=/export/b07/jyang/kaldi-jyang/kaldi/tools/openfst/bin
-export PATH=$FAIRSEQ_PATH:$FAIRSEQ_PATH/scripts:$FAIRSEQ_PATH/utils:$FAIRSEQ_PATH/utils/parallel:$KALDI_LATBIN_PATH:$OPENFST_PATH:$PWD:$PATH
+export PATH=$FAIRSEQ_PATH:$FAIRSEQ_PATH/scripts:$FAIRSEQ_PATH/utils:$FAIRSEQ_PATH/utils/parallel:$KALDI_PATH/latbin:$KALDI_PATH/bin:$OPENFST_PATH:$PWD:$PATH
 unset PYTHONPATH
 export PYTHONPATH=$FAIRSEQ_PATH/../scripts:$PYTHONPATH
