424ef050cda3a147ea3f51483504416087d402f7
Jinyi Yang <jyang126@jhu.edu>
2020-11-17 20:25:10 -0500

    Complete script for preprocess text with subword-nmt, given lattice utterance id lists

--- examples/fisher_callhome_spanish/local/preprocess_subword_nmt_text.sh
+++ examples/fisher_callhome_spanish/local/preprocess_subword_nmt_text.sh
@@ -5,8 +5,7 @@
 
 stage=-1
 source_lang="es"
-#target_lang="en"
-target_lang="es"
+target_lang="en"
 
 case=".lc.rm"
 preprocess_num_workers=40
@@ -35,38 +34,39 @@ if [ $stage -le 1 ]; then
     for idx in $(seq 0 $((${#dsets[@]}-1))); do
         d=${dsets[$idx]}
         fd=${fairseq_dsets[idx]}
-        bpe_dir=$target_bpe_dir/$d.$target_lang/bpe_subword_nmt
+        bpe_dir=$target_bpe_dir/bpe_subword_nmt
         [ -d $bpe_dir ] || mkdir -p $bpe_dir || exit 1
-
         if [ $perform_bpe == "true" ]; then
-            if [ -d $target_text_dir/$d.$target_lang ]; then
-                echo "$(date) Processing BPE tokenization for target  text dataset: $d.$target_lang"
-                input_file=$target_text_dir/$d.$target_lang/text${case}
-                awk 'NR==FNR {a[$1]; next} $1 in a {print $0}' $uttid_dir/$fd.uttid $input_file |\
-                    cut -f 2- -d " " | sed -e 's/\&apos\;/ \&apos\; /g' |\
-                    subword-nmt apply-bpe -c $bpe_code_dir/code.txt \
-                        --vocabulary $bpe_code_dir/vocab.all.txt \
-                        --glossaries "$(cat ${bpe_code_dir}/glossaries.txt)" \
-                        --vocabulary-threshold 1 > $bpe_dir/$d.$target_lang || exit 1
-            else
-                echo "$target_text_dir/$d.$target_lang does not exist !" && exit 1;
-            fi
-            num_src=$(wc -l $uttid_dir/$fd.uttid | cut -d " " -f1)
-            num_tgt=$(wc -l $bpe_dir/$d.$target_lang | cut -d " "  -f1)
-            if [ $num_src -ne $num_tgt ]; then
-                echo "Number of src files not equal to number of target files" && exit 1;
-            fi
+            for lan in $source_lang $target_lang; do
+                if [ -d $target_text_dir/$d.$lan ]; then
+                    echo "$(date) Processing BPE tokenization for target  text dataset: $d.$lan"
+                    input_file=$target_text_dir/$d.$lan/text${case}
+                    awk 'NR==FNR {a[$1]; next} $1 in a {print $0}' $uttid_dir/$fd.uttid $input_file |\
+                        cut -f 2- -d " " | sed -e 's/\&apos\;/ \&apos\; /g' |\
+                        subword-nmt apply-bpe -c $bpe_code_dir/code.txt \
+                            --vocabulary $bpe_code_dir/vocab.all.txt \
+                            --glossaries "$(cat ${bpe_code_dir}/glossaries.txt)" \
+                            --vocabulary-threshold 1 > $bpe_dir/$d.$lan || exit 1
+                else
+                    echo "$target_text_dir/$d.$lan does not exist !" && exit 1;
+                fi
+                num_src=$(wc -l $uttid_dir/$fd.uttid | cut -d " " -f1)
+                num_tgt=$(wc -l $bpe_dir/$d.$lan | cut -d " "  -f1)
+                if [ $num_src -ne $num_tgt ]; then
+                    echo "Number of src files not equal to number of target files" && exit 1;
+                fi
+            done
         fi
     done
         echo "$(date) Fairseq preprocess for target dataset: $d"
-        bpe_dir=$target_bpe_dir/$d.$target_lang
+        bpe_dir=$target_bpe_dir/bpe_subword_nmt
         fairseq-preprocess --source-lang $source_lang --target-lang $target_lang \
             --append-eos-tgt \
             --joined-dictionary \
             --workers $preprocess_num_workers \
             --tgtdict $fairseq_bpe_dict_tgt \
-            --trainpref $target_bpe_dir/train.$target_lang/bpe_subword_nmt/train \
-            --validpref $target_bpe_dir/fisher_dev.$target_lang/bpe_subword_nmt/fisher_dev \
-            --testpref $target_bpe_dir/fisher_dev.$target_lang/bpe_subword_nmt/fisher_dev,$target_bpe_dir/fisher_dev2.$target_lang/bpe_subword_nmt/fisher_dev2,$target_bpe_dir/fisher_test.$target_lang/bpe_subword_nmt/fisher_test,$target_bpe_dir/callhome_devtest.$target_lang/bpe_subword_nmt/callhome_devtest,$target_bpe_dir/callhome_evltest.$target_lang/bpe_subword_nmt/callhome_evltest \
+            --trainpref $bpe_dir/train \
+            --validpref $bpe_dir/fisher_dev \
+            --testpref $bpe_dir/fisher_dev,$bpe_dir/fisher_dev2,$bpe_dir/fisher_test,$bpe_dir/callhome_devtest,$bpe_dir/callhome_evltest \
             --destdir $fairseq_bin_dir || exit 1;
 fi
