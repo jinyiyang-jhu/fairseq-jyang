9c938e9761934eb6917a7ede427e642b4cfab424
Jinyi Yang <jyang126@jhu.edu>
2020-12-01 13:58:26 -0500

    Reorder teh scp files as the given utt list

--- examples/fisher_callhome_spanish/local/lattice_preprocess/filter_plf.sh
+++ examples/fisher_callhome_spanish/local/lattice_preprocess/filter_plf.sh
@@ -13,10 +13,18 @@ done
 #cat $plf_file | awk 'NF!=1' | awk 'NR==FNR {a[$1];next} $1 in a{print $0}' $utt_list - \
 #    > $plf_filtered_file
 cat $plf_file | awk 'NR==FNR {a[$1];next} $1 in a{if (NF !=1 ) {print $0} else {print $1" [noise]"}}' $utt_list - \
-    > $plf_filtered_file
+    > ${plf_filtered_file}.tmp
 
+num_src=$(wc -l $utt_list | cut -d " " -f1)
+num_current=$(wc -l ${plf_filtered_file}.tmp | cut -d " "  -f1)
+if [ $num_src -ne $num_current ]; then
+    echo "Number of filtered utterances differs from given utt list !" && exit 1;
+fi
+
+# Reorder the output as the same order as input file
+awk 'FNR == NR { lineno[$1] = NR; next} {print lineno[$1], $0;}' $utt_list \
+    ${plf_filtered_file}.tmp | sort -k 1,1n | cut -d' ' -f2-  > $plf_filtered_file
 
 num_prev=$(wc -l $plf_file | cut -d " " -f1)
-num_current=$(wc -l $plf_filtered_file | cut -d " " -f1)
-
 echo "Reducing the input file from $num_prev to $num_current" > /dev/stderr
+rm ${plf_filtered_file}.tmp
