e89f095ae9e7fa3deac38e5afffb6988e322c9e2
System User <jyang1@r3n06.cm.gemini>
2021-05-08 22:20:39 -0400

    Add one step to check the sentence numbers for src and tgt, after applying BPE

--- examples/translation/prepare-text.sh
+++ examples/translation/prepare-text.sh
@@ -14,12 +14,12 @@ source $conf
 
 lang="$src-$tgt"
 
-if [ $stage -le 1 ]; then
+if [ $stage -le 0 ]; then
     echo "$(date) Preprocessing ===> Step 1: BPE processing"
     # Use sentencepiece for BPE from raw input text
     mkdir -p $bpe_dir || exit 1;
     if [ ! -f $bpe_dir/bpe.$src.model ]; then
-        echo "No existing bpe $src model, learning BPE from training data ..."
+        echo "$(date) No existing bpe $src model, learning BPE from training data ..."
         spm_train.py --input=$data_dir/train/text.$src \
             --model_prefix=$bpe_dir/bpe.$src \
             --vocab_size=$bpe_src \
@@ -27,11 +27,11 @@ if [ $stage -le 1 ]; then
             --input_sentence_size=$spm_input_sentence_size \
             --shuffle_input_sentence=true \
             --model_type=bpe || exit 1;
-        echo "Learning BPE done: $src"
+        echo "$(date) Learning BPE done: $src"
     fi
 
     if [ ! -f $bpe_dir/bpe.$tgt.model ]; then
-        echo "No existing bpe $tgt model, learning BPE from training data ..."
+        echo "$(date) No existing bpe $tgt model, learning BPE from training data ..."
         spm_train.py --input=$data_dir/train/text.$tgt \
             --model_prefix=$bpe_dir/bpe.$tgt \
             --vocab_size=$bpe_tgt \
@@ -39,16 +39,35 @@ if [ $stage -le 1 ]; then
             --input_sentence_size=$spm_input_sentence_size \
             --shuffle_input_sentence=true \
             --model_type=bpe || exit 1;
-        echo "Learning BPE done: $tgt"
+        echo "$(date) Learning BPE done: $tgt"
     fi
+fi
 
+if [ $stage -le 1 ]; then
     for dset in "${sets[@]}"; do
         for t in $src $tgt; do
-            spm_encode.py --model=$bpe_dir/bpe.$t.model \
+        echo "$(date) Applying BPE to $src: $dset"
+            awk 'BEGIN{i=1}{print i" "$0;++i}' $data_dir/$dset/text.$t |\
+                spm_encode.py --model=$bpe_dir/bpe.$t.model \
                 --output_format=piece \
-                < $data_dir/$dset/text.$t \
-                > $bpe_dir/$dset.bpe.$t || exit 1;
+                > $bpe_dir/$dset.bpe.$t.tmp || exit 1;
+        echo "$(date) Applying BPE to $src: $dset ===> Done !"
         done
+        num_src=$(wc -l $bpe_dir/$dset.bpe.$src.tmp | cut -d " " -f1)
+        num_tgt=$(wc -l $bpe_dir/$dset.bpe.$tgt.tmp | cut -d " " -f1)
+
+        if [ $num_src -ne $num_tgt ]; then
+            echo "Numbef of sentence diffs after BPE for $dset: $src($num_src) v.s. $tgt($num_tgt)"
+            comm -12 <(cut -d " " -f1 test1|sort) <(cut -d " " -f1 test2|sort) > $bpe_dir/$dset.uttid.comm
+            for t in $src $tgt; do
+                awk 'NR==FNR{a[$1];next} $1 in a{print $0}' $bpe_dir/$dset.uttid.comm $bpe_dir/$dset.bpe.$t.tmp |\
+                cut -d " " -f2- > $$bpe_dir/$dset.bpe.$t || exit 1
+            done
+        else
+            for t in $src $tgt; do
+                cut -d " " -f2- $bpe_dir/$dset.bpe.$t.tmp > $bpe_dir/$dset.bpe.$t || exit 1
+            done
+        fi
     done
 fi
 
--- examples/translation/run.sh
+++ examples/translation/run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+prep_stage=-1
 train_stage=-1
 skip_decode=false
+
+[ -f ./path.sh ] && . ./path.sh
+. parse_options.sh
 
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <train-configuration>"
@@ -11,6 +15,6 @@ fi
 conf=$1
 . $conf
 
-prepare-text.sh $conf || exit 1;
+prepare-text.sh --stage $prep_stage $conf || exit 1;
 train.sh --stage $train_stage $conf || exit 1;
 decode.sh --skip-decode $skip_decode $conf || exit 1;
