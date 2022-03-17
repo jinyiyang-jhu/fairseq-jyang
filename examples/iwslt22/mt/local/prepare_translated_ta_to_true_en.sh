cd9788584bb70551ef33f541ee1fec60a7ac6226
System User <jyang1@r3n06.cm.gemini>
2022-03-17 14:51:43 -0400

    Adding scripts for text processing

--- examples/iwslt22/mt/local/prepare_translated_ta_to_true_en.sh
+++ examples/iwslt22/mt/local/prepare_translated_ta_to_true_en.sh
@@ -1,45 +1,142 @@
 #!/bin/bash
 
 stage=-1
-ori_tgt_dir="data/msa-en_processed/spm2000/split8"
-decode_dir="exp_msa-ta_bpe2000_msa2ta_msa_translated_ta_true/decode_spm2000_msa2en_manual_clean_train"
-outdir="data/ta-en_ta_translated_en_true"
-spm_mdl=
 
 src_lan="ar" # Not used for training, just for sanity check
 tgt_lan="en"
 hyp_lan="ta"
 
-src_case="rm"
+src_case="tc.rm"
 tgt_case="lc.rm"
-hyp_case="rm"
-set="train"
+hyp_case="tc.rm"
+trainset="train"
+num_bpe=2000
 
-# src_path=exp_msa-ta_bpe2000_msa2ta_msa_translated_ta_true/decode_spm2000_msa2en_manual_clean_train/split1/src.ar.text
-# hyp_path=exp_msa-ta_bpe2000_msa2ta_msa_translated_ta_true/decode_spm2000_msa2en_manual_clean_train/split1/hyp.ta.text
-# tgt_path=data/msa-en_processed/spm2000/split8/1/train.bpe.ar-en.en
-# outdir=data/ta-en_ta_translated_en_true
+ori_tgt_dir="data/msa-en_processed/spm2000/split8"
+decode_dir="exp_msa-ta_bpe2000_msa2ta_msa_translated_ta_true/decode_spm2000_msa2en_manual_clean_train"
+datadir="data/ta-en_ta_translated_en_true"
+bindir="exp_ta-en_ta_translated_en_true/bin_${hyp_lan}2${tgt_lan}"
+hyp_lan_bpe_mdl="data/msa-en_processed/spm2000/${src_lan}_bpe_spm2000/bpe.model"
+tgt_lan_bpe_mdl="data/msa-en_processed/spm2000/${tgt_lan}_bpe_spm2000/bpe.model"
 
-mkdir -p $outdir || exit 1
+srcdict="exp_msa-ta_bpe2000_msa2ta_msa_translated_ta_true/bin_ar2ta/dict.ta.txt"
+tgtdict="exp_msa-en_bpe2000/bin_ar2en/dict.en.txt"
+
+# dev
+dev_hyp_lan_text="data/ta-en_clean/dev/text.${hyp_case}.${hyp_lan}"
+dev_tgt_lan_text="data/ta-en_clean/dev/text.${tgt_case}.${tgt_lan}"
+
+# test
+test_hyp_lan_text="data/ta-en_clean/test1/text.${hyp_case}.${hyp_lan}"
+test_tgt_lan_text="data/ta-en_clean/test1/text.${tgt_case}.${tgt_lan}"
+
+. path.sh
 
 if [ $stage -le 0 ]; then
     echo "$(date) Stage 1: Filtering src/hyp/tgt text files"
-    for i in $(seq 1 8); do
+    for i in $(seq 8); do
     (
         echo "$(date) Processing split ${i}"
+        grep ^D ${decode_dir}/split${i}/decode.${i}.log | grep -v "fairseq.logging.progress_bar" | awk -F"\t" '{print $1"\t"$3}' \
+            > $decode_dir/split${i}/hyp.${hyp_lan}.undetok.text || exit 1
+        
         src_path=${decode_dir}/split${i}/src.${src_lan}.text
-        hyp_path=${decode_dir}/decode_spm2000_msa2en_manual_clean_${set}/split${i}/hyp.${hyp_lan}.text
-        tgt_path=${ori_tgt_dir}/${i}/${set}.bpe.${src_lan}-${tgt_lan}.${tgt_lan}
+        hyp_path=${decode_dir}/split${i}/hyp.${hyp_lan}.undetok.text
+        tgt_path=${ori_tgt_dir}/${i}/${trainset}.bpe.${src_lan}-${tgt_lan}.${tgt_lan}
+        outdir=${datadir}/${trainset}/split${i}
+        mkdir -p ${outdir} || exit 1
+        echo "$(date) Filtering hyp and target: split ${i}"
+        for f in ${src_path} ${hyp_path} ${tgt_path}; do
+            if [ ! -f $f ]; then
+                echo "No such file: $f"
+                exit 1
+            fi
+        done
 
         python local/sort_fairseq_gen_hyp_to_ref.py \
             --src_text $src_path \
             --hyp_text $hyp_path \
             --tgt_txt $tgt_path \
-            --out_src "${outdir}/${set}.bpe.${src_lan}-${tgt_lan}.${tgt_lan}" \
-            --out_hyp "${outdir}/${set}.${hyp_lan}-${tgt_lan}.${hyp_lan}.${hyp_case}" \
-            --out_tgt "${outdir}/${set}.${hyp_lan}-${tgt_lan}.${tgt_lan}.${tgt_case}" \
+            --out_src ${outdir}/text.${src_case}.${src_lan} \
+            --out_hyp ${outdir}/text.${hyp_case}.${hyp_lan} \
+            --out_tgt ${outdir}/text.bpe.${tgt_case}.${tgt_lan}
     ) &
     done
     wait
+    
+    for f in "text.${src_case}.${src_lan}" "text.${hyp_case}.${hyp_lan}"; do
+        if [ -f ${datadir}/${trainset}/${f} ]; then
+            rm ${datadir}/${trainset}/${f}
+        fi
+    done
+
+    if [ -f "${datadir}/spm${num_bpe}/${trainset}.bpe.${hyp_lan}-${tgt_lan}.${tgt_lan}" ]; then
+        rm  "${datadir}/spm${num_bpe}/${trainset}.bpe.${hyp_lan}-${tgt_lan}.${tgt_lan}"
+    fi
+    mkdir -p ${datadir}/spm${num_bpe} || exit 1;
+    for n in $(seq 8); do
+        outdir=${datadir}/${trainset}/split${n}
+        cut -d " " -f2- "${outdir}/text.${src_case}.${src_lan}"  >> "${datadir}/${trainset}/text.${src_case}.${src_lan}"
+        cut -d " " -f2- "${outdir}/text.${hyp_case}.${hyp_lan}"  >> "${datadir}/${trainset}/text.${hyp_case}.${hyp_lan}"
+        cut -d " " -f2- "${outdir}/text.bpe.${tgt_case}.${tgt_lan}"  >> "${datadir}/spm${num_bpe}/${trainset}.bpe.${hyp_lan}-${tgt_lan}.${tgt_lan}"
+    done
 fi
 
+if [ $stage -le 1 ]; then
+    echo "$(date) Preprocess train/dev/test: normalization, BPE convertion, and binarization"
+    echo "$(data) Preprocess: train"
+    for s in ${trainset}; do
+        text="${datadir}/${s}/text.${hyp_case}.${hyp_lan}"
+        detok="${datadir}/${s}/text.${hyp_case}.${hyp_lan}.detok"
+        tok="${datadir}/${s}/text.${hyp_case}.${hyp_lan}.tok"
+        bpe="${datadir}/spm${num_bpe}/${s}.bpe.${hyp_lan}-${tgt_lan}.${hyp_lan}"
+
+        detokenizer.perl -q -no-escape < ${text} > ${detok} || exit 1;
+        tokenizer.perl -q -no-escape < ${detok} > ${tok} || exit 1;
+
+        spm_encode \
+            --model="${hyp_lan_bpe_mdl}" \
+            --output_format=piece \
+            < ${tok}} \
+            > ${bpe} || exit 1;
+    done
+
+    for s in "dev" "test1"; do
+        for lan in ${hyp_lan} ${tgt_lan}; do
+            if [ "${lan}" == "${hyp_lan}" ]; then
+                lan_case=${src_case}
+                bpe_mdl=${hyp_lan_bpe_mdl}
+            else
+                lan_case=${tgt_case}
+                bpe_mdl=${tgt_lan_bpe_mdl}
+            fi
+            echo "$(data) Preprocess: ${s}:${lan}"
+
+            text="${datadir}/${s}/text.${lan_case}.${lan}"
+            detok="${datadir}/${s}/text.${lan_case}.${lan}.detok"
+            tok="${datadir}/${s}/text.${lan_case}.${lan}.tok"
+            bpe="${datadir}/spm${num_bpe}/${s}.bpe.${hyp_lan}-${tgt_lan}.${lan}"
+
+            tokenizer.perl -q -no-escape < ${detok} > ${tok} || exit 1;
+
+            spm_encode \
+                --model="${bpe_mdl}" \
+                --output_format=piece \
+                < ${tok}} \
+                > ${bpe} || exit 1;
+        done
+    done
+
+    echo "$(date '+%Y-%m-%d %H:%M:%S') Binarizing data"
+    trainpref="${datadir}/spm${num_bpe}/${trainset}.bpe.${hyp_lan}-${tgt_lan}"
+    validpref="${datadir}/spm${num_bpe}/dev.bpe.${hyp_lan}-${tgt_lan}"
+    testpref="${datadir}/spm${num_bpe}/test1.bpe.${hyp_lan}-${tgt_lan}"
+
+    fairseq-preprocess \
+        --source-lang ${hyp_lan} --target-lang ${tgt_lan} \
+        --srcdict ${srcdict} --tgtdict ${tgtdict} \
+        --trainpref $trainpref --validpref $validpref --testpref $testpref \
+        --destdir ${bindir} --thresholdtgt 0 --thresholdsrc 0 \
+        --workers 16 || exit 1;
+
+fi
