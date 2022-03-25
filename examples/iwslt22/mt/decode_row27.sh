2e7c63f395a999f08e4fee727ba623124cbe84aa
System User <jyang1@r3n06.cm.gemini>
2022-03-24 22:18:27 -0400

    Sort the reference file according to the provided ASR hyp; used for ensuring that blind_test order unchanged

---
+++ examples/iwslt22/mt/decode_row27.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+stage=-1
+nj=1 # no greater than qquota
+
+############ Change #############
+#src_case=".tc.rm"
+#set_name="gold_transcript"
+#path_to_eval_data="data/ta-en_clean"
+
+
+src_case=""
+set_name="row15"
+path_to_eval_data="amir_asr/row15"
+############ Change #############
+path_to_bpe_mdl=/home/hltcoe/jyang1/tools/espnet/egs2/iwslt22_dialect/st1/data_clean/spm_bpe_4000/ta_bpe_spm4000/bpe.model
+mdl_name="row27"
+mdl_dir=/exp/jyang1/exp/IWSLT22/TA-EN/MT/fairseq/exp_clean_spm4000
+
+
+src_lan="ta"
+tgt_lan="en"
+tgt_case="tc"
+decode_tgt_lan="en"
+
+skip_decode="False"
+hyp_is_scp="True"
+
+sets=("dev" "test1" "blind_eval")
+path_to_ref_data="data/ta-en_clean"
+nbpe=4000
+
+path_to_dict_dir=${mdl_dir}/bin_${src_lan}2${tgt_lan}
+path_to_mdl=${mdl_dir}/checkpoints/checkpoint_best.pt
+
+. path.sh
+. cmd.sh
+. parse_options.sh
+
+if [ ${skip_decode} != "True" ]; then
+    for dset in ${sets[@]}; do
+        path_to_eval_src=${path_to_eval_data}/${dset}/text${src_case}.${src_lan}
+        path_to_eval_tgt=${path_to_ref_data}/${dset}/text.${tgt_case}.${tgt_lan}
+        if [ $dset != "blind_eval" ]; then
+            num_src_lines=$(wc -l ${path_to_eval_src} | cut -d" " -f1)
+            num_tgt_lines=$(wc -l ${path_to_eval_tgt} | cut -d" " -f1)
+            if [ ${num_src_lines} -ne ${num_tgt_lines} ]; then
+                echo "Line mismatch: src ($num_src_lines) vs tgt ($num_tgt_lines)"
+                exit 1;
+            fi
+        fi
+
+        decode_dir=${mdl_dir}/decode_${set_name}_${dset}_interactive
+        mkdir -p ${decode_dir} || exit 1
+
+        cat ${path_to_eval_src} | cut -d" " -f2- | tokenizer.perl -q -no-escape > ${decode_dir}/${src_lan}.txt
+        cat ${path_to_eval_src} | cut -d" " -f1 > ${decode_dir}/${src_lan}.uttids
+
+        if [ $stage -le 0 ]; then
+            echo "$(date '+%Y-%m-%d %H:%M:%S') Apply BPE to source data: ${dset}-${src_lan}"
+            spm_encode \
+            --model=${path_to_bpe_mdl} \
+            --output_format=piece \
+            < ${decode_dir}/${src_lan}.txt \
+            > ${decode_dir}/${src_lan}.bpe.txt || exit 1;
+        fi
+
+        echo "$(date '+%Y-%m-%d %H:%M:%S') Decoding ${src_lan}-${tgt_lan}:${src_lan}: ${dset}"
+
+        #${decode_cmd} --gpu 1 ${decode_dir}/decode.log \
+            # cat ${decode_dir}/${src_lan}.bpe.txt \| \
+        cat ${decode_dir}/${src_lan}.bpe.txt | \
+            fairseq-interactive ${path_to_dict_dir} \
+                --source-lang "${src_lan}" --target-lang "${tgt_lan}" \
+                --task translation \
+                --path ${path_to_mdl}\
+                --batch-size 256 \
+                --fix-batches-to-gpus \
+                --beam 5 \
+                --buffer-size 2000 \
+                --remove-bpe=sentencepiece > ${decode_dir}/decode.log
+        
+        grep ^D $decode_dir/decode.log | cut -f3 | \
+            detokenizer.perl -q -no-escape > ${decode_dir}/hyp.${decode_tgt_lan}.txt || exit 1
+        grep ^S $decode_dir/decode.log | cut -f2- | \
+            detokenizer.perl -q -no-escape > ${decode_dir}/src.${src_lan}.txt || exit 1  
+
+        if [ $dset != "blind_eval" ]; then
+
+            path_to_eval_tgt_sorted=${path_to_eval_data}/${dset}/text.${tgt_case}.${tgt_lan}.sorted
+            paste -d " " ${decode_dir}/${src_lan}.uttids ${decode_dir}/hyp.${decode_tgt_lan}.txt \
+                > ${decode_dir}/hyp.${decode_tgt_lan}.text
+
+            python local/sort_src_to_tgt_order.py \
+                --src_text ${decode_dir}/hyp.${decode_tgt_lan}.text \
+                --hyp_in_text ${path_to_eval_tgt} \
+                --hyp_out_text ${path_to_eval_tgt_sorted} || exit 1;
+    
+
+            cat ${path_to_eval_tgt_sorted} | cut -d" " -f2- | detokenizer.perl -q -no-escape > ${decode_dir}/ref.${tgt_lan}.sorted.txt
+            cat ${path_to_eval_tgt_sorted} | cut -d" " -f1 > ${decode_dir}/${tgt_lan}.sorted.uttids
+            diff_utts=$(diff "${decode_dir}/${src_lan}.uttids" "${decode_dir}/${tgt_lan}.sorted.uttids")
+            if [ ! -z "${diff_utts}" ]; then
+                echo "Utt order mismatch: src [ ${decode_dir}/${src_lan}.uttids ] vs tgt [ ${decode_dir}/${tgt_lan}.sorted.uttids ]"
+                exit 1
+            fi
+
+            sacrebleu ${decode_dir}/ref.${tgt_lan}.sorted.txt -i ${decode_dir}/hyp.${decode_tgt_lan}.txt -m bleu -lc > ${decode_dir}/results.txt || exit 1
+            cp ${decode_dir}/hyp.${decode_tgt_lan}.text ${path_to_eval_data}/${dset}/hyp.${mdl_name}.text
+            cp ${decode_dir}/results.txt ${path_to_eval_data}/${dset}/bleus.${mdl_name}.txt
+        else
+            cp ${decode_dir}/hyp.${decode_tgt_lan}.text ${path_to_eval_data}/${dset}/hyp.${mdl_name}.text
+        fi
+        echo "$(date '+%Y-%m-%d %H:%M:%S') Evaluation done !"
+    done
+fi
+
+
+    
